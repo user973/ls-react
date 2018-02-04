cab1ef02f6fce3df9337c5034eaf0578affe1de8
just3is@mail.ru <just3is@mail.ru>
2018-02-04 19:44:35 +0300

    homework 4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
-import './App.css';
+import {Link, Switch, Route, Redirect} from 'react-router-dom';
+import Auth from './Auth';
+import Private from './Private';
+import Public from './Public';
+import Home from './Home';
 import {addListener, removeListener, isAuthorized} from './AuthorizeApi';
+import './App.css';
+
 
 class App extends Component {
   state = {
@@ -20,7 +26,27 @@ class App extends Component {
   };
 
   render() {
-    return null;
+    //className='App'
+    const {isAuthorized} = this.state;
+    return(
+      <div>
+        <nav>
+          <ul>
+            <li><Link to='/auth'>Войти</Link></li>
+            <li><Link to='/private'>Секретная страница</Link></li>
+            <li><Link to='/public'>Публичная страница</Link></li>
+            <li><Link to='/'>Главная</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/auth' component={Auth} />
+          {isAuthorized&&<Route path='/private' render={() => isAuthorized ? <Private /> : <Redirect to='/auth' />} />}
+          <Route path='/public' component={Public} />
+          {!isAuthorized&&<Route path='/*' render={(path) => path.match.url === '/private' ?  <Redirect to='/auth' /> : <Home />} />}
+        </Switch>
+      </div>
+    );
   }
 }
 
--- src/Auth.js
+++ src/Auth.js
@@ -1,9 +1,49 @@
 import React, {Component} from 'react';
+import {authorizeUser, isAuthorized} from './AuthorizeApi';
+import Redirect from 'react-router-dom/Redirect';
 
-class Auth extends Component {
-  render() {
-    return null;
-  }
-}
+export default class Auth extends Component {
 
-export default Auth;
+    state = {
+        email: '',
+        password: '',
+        isAuthorized: isAuthorized,
+        showError: false
+    };
+
+    handleSubmit = () => {
+        const {email,password} = this.state;
+        const isAuthorized = authorizeUser(email,password);
+        this.setState({
+            isAuthorized,
+            showError:!isAuthorized
+        });
+    };
+
+    handleChange = (e) => {
+        const {target: {
+            value, name
+        }} = e;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    render() {
+        const {email,password,isAuthorized,showError} = this.state;
+        if (isAuthorized) {
+            return <Redirect to='/' />;
+        }
+        return (
+            <div>
+                <div>
+                    <input name='email' placeholder='email' onChange={this.handleChange} value={email} />
+                    <input name='password' placeholder='password' onChange={this.handleChange} value={password} />
+                    {showError&&<p className='error'>Неверный пароль и/или почта.</p>}
+                </div>
+                <button onClick={this.handleSubmit}>Submit</button>
+            </div>
+        );
+    };
+    
+};
