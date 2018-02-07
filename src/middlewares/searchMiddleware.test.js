12c3e2a4a42099035009172d4ed05b1e1c86ee4b
Artem Samofalov <dex157@gmail.com>
2018-02-07 15:59:48 +0300

    fix tests

--- src/middlewares/searchMiddleware.test.js
+++ src/middlewares/searchMiddleware.test.js
@@ -8,17 +8,17 @@ search.mockImplementation(() => new Promise(r => r(['test', 'data'])));
 describe('searchMiddleware', () => {
   it('Вызывается функция search из модуля ../api если приходит action с типом SEARCH_REQUEST', () => {
     const storeMock = {
-      dispatch: jest.fn().mockImplementation(() => '')
+      dispatch: jest.fn().mockImplementation(() => ''),
     };
-    searchMiddleware(storeMock)(jest.fn())({type: 'SEARCH_REQUEST'});
+    searchMiddleware(storeMock)(jest.fn())({type: 'SEARCH_REQUEST', payload: {}});
     expect(search).toHaveBeenCalledTimes(1);
   });
   it('Если promise resolved то middleware отправляет экшен SEARCH_SUCCESS', done => {
     const dispatchMock = jest.fn();
     const storeMock = {
-      dispatch: dispatchMock
+      dispatch: dispatchMock,
     };
-    searchMiddleware(storeMock)(jest.fn())({type: 'SEARCH_REQUEST'});
+    searchMiddleware(storeMock)(jest.fn())({type: 'SEARCH_REQUEST', payload: {}});
     setTimeout(() => {
       expect(dispatchMock).toHaveBeenCalledTimes(1);
       done();
